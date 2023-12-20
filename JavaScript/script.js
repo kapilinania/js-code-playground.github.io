9d6b33868f8a1453abf04680832278196d6b0b34
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-12-20 16:50:26 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+console.log("welcome to js")
+const accountId = 256  //it never will be changed 
+let accountEmail = "inaniyakapil2000@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //this is not good 
+let accountState ;
+
+//accountId = 2
+/*
+we are not use var because of issue in block scope and function scope
+*/
+accountEmail="bhaikapil175@gmail.com"
+accountPassword = "1212"
+accountCity = "Jodhpur"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
