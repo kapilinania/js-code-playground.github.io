e64e20c1abba0d9ad7acc0fa3629a0db781df148
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-12-21 08:56:37 +0000

    commit

---
+++ JavaScript/day2/datatype.js
@@ -0,0 +1,31 @@
+// here we are learn Datatypes
+
+"use strict"  //treat all js code as newer version
+
+// alert("Hello")
+// alert(3+3)  //we are useing node js not browser
+console.log(3+3)
+console.log("kapil")
+
+let name ="kapil"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => "" or simple ''
+//boolean => true , false
+//null => standalone value  //represent to khali values
+//underfinded => jb humn value define nhi ki hai
+//symbol => unique
+
+// these all are premitive data type  
+
+//object
+console.log(typeof "kapil")
+console.log(typeof null) //it is object
+console.log(typeof undefined) //it is undefined
+let state = null //it is null
+let city; //it is underfined
+
+
---
+++ JavaScript/day2/datatypes_02.js
@@ -0,0 +1,25 @@
+//conversion 
+let score = 33
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+//"33" => 33
+//"33abc" => NaN
+// true=>1; false=>0
+
+
+let isLoggedIn = "kapil"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true; 0=> false
+//"" = false
+// "kapil" = true
+
+let someNuber = 82
+let stringNumber = String(someNuber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
